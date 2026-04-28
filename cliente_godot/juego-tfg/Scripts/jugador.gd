f6a1830b5d0a0bef7ca6cf03fcaec21e083cc438
Erik Doldan <erik.doldan@fpcoruna.afundacion.org>
2026-04-28 18:40:12 +0200

    Primeros avances en el nivel 1 creando con el tileset los layers de fondo suelo y decorado, ademas añadi ya las animaciones del personaje de idle run y jump

--- cliente_godot/juego-tfg/Scripts/jugador.gd
+++ cliente_godot/juego-tfg/Scripts/jugador.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
-# Hemos aumentado la velocidad y el salto para que sea más dinámico
-const VELOCIDAD = 400.0
-const FUERZA_SALTO = -650.0 
-const MULTIPLICADOR_GRAVEDAD = 2.5 # ¡La clave para que no flote!
+# Referencia al nodo de animación
+# Asegúrate de que el nombre coincida con el de tu escena ($AnimatedSprite2D)
+@onready var sprite = $AnimatedSprite2D
+
+const VELOCIDAD = 150.0
+const FUERZA_SALTO = -300.0 
+const MULTIPLICADOR_GRAVEDAD = 1.2 
 
 var gravedad = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	# 1. APLICAR GRAVEDAD (Ahora multiplicada para caer más rápido)
+	# 1. APLICAR GRAVEDAD
 	if not is_on_floor():
 		velocity.y += gravedad * MULTIPLICADOR_GRAVEDAD * delta
 
@@ -21,8 +24,25 @@ func _physics_process(delta):
 	
 	if direccion != 0:
 		velocity.x = direccion * VELOCIDAD
+		# Invertir el sprite según la dirección
+		sprite.flip_h = (direccion < 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, VELOCIDAD)
 
-	# 4. APLICAR MOVIMIENTO
+	# 4. GESTIÓN DE ANIMACIONES
+	actualizar_animaciones(direccion)
+
+	# 5. APLICAR MOVIMIENTO
 	move_and_slide()
+
+func actualizar_animaciones(direccion):
+	if not is_on_floor():
+		# Si está en el aire, reproducimos Jump
+		sprite.play("Jump")
+	else:
+		if direccion != 0:
+			# Si se mueve en el suelo, reproducimos Run
+			sprite.play("Run")
+		else:
+			# Si está quieto en el suelo, reproducimos Idle
+			sprite.play("Idle")
