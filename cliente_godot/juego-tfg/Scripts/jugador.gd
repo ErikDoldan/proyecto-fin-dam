ba8e9af5eb4b5e2b8d5fc3ca591dc69aee3957f1
Erik Doldan <erik.doldan@fpcoruna.afundacion.org>
2026-05-06 00:21:43 +0200

    Avances sistema de combate (el slime me pega y me empuja), creacion vbox container para los corazones y funcionalidad de que si me pegan me baja vida

--- cliente_godot/juego-tfg/Scripts/jugador.gd
+++ cliente_godot/juego-tfg/Scripts/jugador.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 # Referencia al nodo de animación
 # Asegúrate de que el nombre coincida con el de tu escena ($AnimatedSprite2D)
 @onready var sprite = $AnimatedSprite2D
+var vidas = 3
+var recibiendo_dano = false 
 
 const VELOCIDAD = 150.0
 const FUERZA_SALTO = -300.0 
@@ -10,8 +12,16 @@ const MULTIPLICADOR_GRAVEDAD = 1.2
 
 var gravedad = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var contenedor_corazones = $UI/HBoxContainer
+
 func _physics_process(delta):
-	# 1. APLICAR GRAVEDAD
+	
+	if recibiendo_dano:
+		if not is_on_floor():
+			velocity.y += gravedad * MULTIPLICADOR_GRAVEDAD * delta
+		move_and_slide()
+		return
+		
 	if not is_on_floor():
 		velocity.y += gravedad * MULTIPLICADOR_GRAVEDAD * delta
 
@@ -46,3 +56,42 @@ func actualizar_animaciones(direccion):
 		else:
 			# Si está quieto en el suelo, reproducimos Idle
 			sprite.play("Idle")
+func recibir_dano(posicion_x_enemigo):
+	if recibiendo_dano:
+		return 
+	vidas -= 1
+	recibiendo_dano = true
+	print("Auch! Vidas restantes: ", vidas) 
+	actualizar_corazones()
+	
+	var direccion_empuje = 1
+	if global_position.x < posicion_x_enemigo:
+		direccion_empuje = -1
+		
+	
+	velocity.x = direccion_empuje * 150 
+	velocity.y = -200 
+	
+	
+	sprite.play("Hit")
+	
+	await get_tree().create_timer(0.4).timeout
+	
+	recibiendo_dano = false
+	
+func actualizar_corazones():
+	
+	var array_corazones = contenedor_corazones.get_children()
+	
+	
+	for i in range(array_corazones.size()):
+	
+		if i < vidas:
+			array_corazones[i].visible = true
+		else:
+			array_corazones[i].visible = false
+			
+	
+	if vidas <= 0:
+		print("¡GAME OVER!")
+		# Aquí luego meteremos la lógica de morir de verdad (reiniciar nivel, menú, etc.)
--- cliente_godot/juego-tfg/Scripts/slime.gd
+++ cliente_godot/juego-tfg/Scripts/slime.gd
@@ -8,30 +8,38 @@ var esta_muerto = false
 @onready var ray_cast_l: RayCast2D = $RayCastL
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox_arriba: Area2D = $HitboxArriba
+@onready var hitbox_cuerpo: Area2D = $HitboxCuerpo
 
 func _ready() -> void:
 	
 	
 	# Conecto la señal de la hitbox 
 	hitbox_arriba.body_entered.connect(_on_hitbox_arriba_body_entered)
-
+	hitbox_cuerpo.body_entered.connect(_on_hitbox_cuerpo_body_entered)
+	
 func _process(delta: float) -> void:
-	# Si el bicho está muerto, fuera
 	if esta_muerto:
 		return
 
+	# RAYCAST DERECHO 
 	if ray_cast_r.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
+		var objeto_chocado_r = ray_cast_r.get_collider()
+		# Si choca con algo y ese algo NO es el Jugador, cambia de dirección
+		if objeto_chocado_r and objeto_chocado_r.name != "Jugador":
+			direction = -1
+			animated_sprite.flip_h = true
+
+	# RAYCAST IZQUIERDO 
 	if ray_cast_l.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
+		var objeto_chocado_l = ray_cast_l.get_collider()
 		
+		if objeto_chocado_l and objeto_chocado_l.name != "Jugador":
+			direction = 1
+			animated_sprite.flip_h = false
+			
 	position.x += direction * speed * delta
 
-func _on_hitbox_arriba_body_entered(body: Node2D) -> void:
-	if body.name == "Jugador" and not esta_muerto:
-		morir(body)
+
 
 func morir(jugador: Node2D) -> void:
 	esta_muerto = true
@@ -46,3 +54,16 @@ func morir(jugador: Node2D) -> void:
 	
 	# Borro al bicho 
 	queue_free()
+	
+func _on_hitbox_arriba_body_entered(body: Node2D) -> void:
+	if body.name == "Jugador" and not esta_muerto:
+		#Solo muere si el jugador está cayendo
+		if body.velocity.y > 0:
+			morir(body)
+
+func _on_hitbox_cuerpo_body_entered(body: Node2D) -> void:
+	if body.name == "Jugador" and not esta_muerto:
+		
+		if body.velocity.y <= 0:
+			if body.has_method("recibir_dano"):
+				body.recibir_dano(global_position.x)
