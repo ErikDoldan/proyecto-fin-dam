0505fa006bf437ccdee2a46e19440e9506ec6258
Erik Doldan <erik.doldan@fpcoruna.afundacion.org>
2026-05-18 01:09:01 +0200

    Enemigo funcional

--- cliente_godot/juego-tfg/Scripts/bola_fuego.gd
+++ cliente_godot/juego-tfg/Scripts/bola_fuego.gd
@@ -21,6 +21,18 @@ func _physics_process(delta):
 	velocity.x = velocidad_x * direccion
 	move_and_slide()
 
+	# --- NUEVO: DETECTAR CHOQUES CONTRA ENEMIGOS ---
+	for i in get_slide_collision_count():
+		var colision = get_slide_collision(i)
+		var objeto_chocado = colision.get_collider()
+		
+		# Verificamos si el objeto con el que chocamos tiene la función de daño
+		if objeto_chocado and objeto_chocado.has_method("sufrir_dano"):
+			objeto_chocado.sufrir_dano(1, global_position.x)
+			explotar() # La bola explota al darle al enemigo
+			return # Cortamos aquí para que no siga calculando físicas
+	# -----------------------------------------------
+
 	if is_on_floor():
 		velocity.y = fuerza_rebote
 
@@ -30,14 +42,11 @@ func _physics_process(delta):
 func explotar():
 	ha_explotado = true
 	
-
 	$CollisionShape2D.set_deferred("disabled", true)
-	
 	$Sprite2D.visible = false
-	
 	$CPUParticles2D.emitting = false 
-	
 	$Explosion.emitting = true
+	
 	await get_tree().create_timer(0.5).timeout
 	queue_free()
 	
